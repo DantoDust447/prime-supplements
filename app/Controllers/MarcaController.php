8a112f773542f0e077e4922fde8616ea3781a8f9
Hamilton613 <leonelcalderon939@gmail.com>
2025-10-08 16:01:44 -0600

    se agrego marcas

--- app/Controllers/MarcaController.php
+++ app/Controllers/MarcaController.php
@@ -10,4 +10,35 @@ class MarcaController extends BaseController
         $datos['marcas'] = $marcas->findAll();
         return view('vista_marcas', $datos); 
     }
+
+     public function index() 
+    {
+        $marcas = new MarcasModel();
+        $datos['datos'] = $marcas->findAll();
+        return view('vista_marcas_empleado', $datos);
+    }
+
+    public function agregar() 
+    {
+        $marcas = new MarcasModel();
+        $datos = [
+            'marca_nombre' => $this->request->getVar('marca_nombre'),
+            'descripcion' => $this->request->getVar('descripcion')
+        ];
+        $marcas->insert($datos);
+        return redirect()->to(base_url('marcas'));
+    }
+
+    public function eliminar($marca_id) 
+    {
+        $marcas = new MarcasModel();
+        $marcas->delete($marca_id);
+        return redirect()->to('marca');
+    }
+    
 }
+
+    
+
+
+    
--- app/Controllers/ProductoController.php
+++ app/Controllers/ProductoController.php
@@ -14,4 +14,54 @@ class ProductoController extends BaseController
         return view('vista_productos', $datos);
         
     }
+
+/*empleados*/
+    public function index(): string 
+    {
+        $producto = new ProductosModel();
+
+        $datos['datos'] = $producto->findAll();
+        return view('vista_producto_empleado', $datos);
+    }
+    public function eliminar($producto_id)
+    {
+        $producto = new ProductosModel();
+        $producto->delete($producto_id);
+        return redirect()->to('productos');
+    }
+    public function agregar()
+    {
+        $producto = new ProductosModel();
+        $datos = [
+            'nombre' => $this->request->getVar('nombre'),
+            'marca_id' => $this->request->getVar('marca_id'),
+            'descripcion' => $this->request->getVar('descripcion'),
+            'precio' => $this->request->getVar('precio'),
+            'cantidad_peso' => $this->request->getVar('cantidad_peso'),
+            'categoria_id' => $this->request->getVar('categoria_id'),
+        ];
+        $producto->insert($datos);
+        return redirect()->to('productos');
+    }
+    public function buscar($producto_id)
+    {
+        $producto = new ProductosModel();
+        $datos['datos'] = $producto->where('producto_id', $producto_id)->first();
+        return view('vistaModificarProducto', $datos);
+    }
+    
+    public function modificar()
+    {
+        $producto = new ProductosModel();
+        $datos = [
+            'producto_id' => $this->request->getPost('producto_id'),
+            'nombre' => $this->request->getPost('nombre'),
+            'marca_id' => $this->request->getPost('marca_id'),
+            'descripcion' => $this->request->getPost('descripcion'),
+            'precio' => $this->request->getPost('precio'),
+            'categoria_id' => $this->request->getPost('categoria_id'),
+        ];
+        $producto->update($datos['producto_id'], $datos);
+        return redirect()->to('productos');
+    }
 }
